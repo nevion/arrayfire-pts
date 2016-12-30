761ad9b6e1d6146012379d7a772f91ecab4e9936
Jason Newton <nevion@gmail.com>
2016-12-30 13:07:44 -0500

    add probe benchmarks script

---
+++ probe_benchmarks.rb
@@ -0,0 +1,16 @@
+output = `./bin/benchmark_cpu -l|grep -v JIT|awk ' NR > 10 { print $2 } NR <= 10 { next }'`
+#puts(output)
+compute_device = ENV['COMPUTE_DEVICE'] || '0'
+puts("compute device #{compute_device}")
+output.lines.map(&:chomp).each do |benchmark|
+    cmd = "timeout -s SIGKILL 15 ./bin/benchmark_opencl -d #{compute_device} -b #{benchmark} --use-max-problemspace"
+    #cmd = "/bin/benchmark_opencl -d #{compute_device} -b #{benchmark}"
+    puts(cmd)
+    output = `#{cmd}`
+    if $?.exitstatus != 0
+        open('failures.txt', 'a') { |f|
+              f.puts "#{benchmark}"
+              f.puts output
+        }
+    end
+end
